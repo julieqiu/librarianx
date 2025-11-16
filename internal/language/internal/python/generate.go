504fa69bba38dc107d89030662518f43a33fb900
Julie Qiu <julie@golang.org>
2025-11-16 18:41:42 -0500

    internal/python: add package doc

--- internal/language/internal/python/generate.go
+++ internal/language/internal/python/generate.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package python provides functionality for generating Python client libraries.
 package python
 
 import (
