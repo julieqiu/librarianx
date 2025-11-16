86f29ac32ea7e3031cdaddedb115f6bf8ef03005
Julie Qiu <julie@golang.org>
2025-11-16 11:36:17 -0500

    cmd/librarian-migrate: add legacy types

---
+++ cmd/librarian-migrate/types.go
@@ -0,0 +1,168 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+// LegacyState represents the old .librarian/state.yaml file.
+type LegacyState struct {
+	// Image is the generator container image reference.
+	Image string `yaml:"image"`
+
+	// Libraries is the list of libraries in the repository.
+	Libraries []LegacyStateLibrary `yaml:"libraries"`
+}
+
+// LegacyStateLibrary represents a library in state.yaml.
+type LegacyStateLibrary struct {
+	// ID is the unique identifier for the library.
+	ID string `yaml:"id"`
+
+	// Version is the last released version.
+	Version string `yaml:"version,omitempty"`
+
+	// LastGeneratedCommit is the googleapis commit hash at last generation.
+	LastGeneratedCommit string `yaml:"last_generated_commit,omitempty"`
+
+	// APIs is the list of APIs that are part of this library.
+	APIs []LegacyAPI `yaml:"apis"`
+
+	// SourceRoots is the list of directories where code is generated.
+	SourceRoots []string `yaml:"source_roots"`
+
+	// PreserveRegex is the list of patterns for files to preserve.
+	PreserveRegex []string `yaml:"preserve_regex,omitempty"`
+
+	// RemoveRegex is the list of patterns for files to remove.
+	RemoveRegex []string `yaml:"remove_regex,omitempty"`
+
+	// ReleaseExcludePaths is the list of paths to exclude from releases.
+	ReleaseExcludePaths []string `yaml:"release_exclude_paths,omitempty"`
+
+	// TagFormat is the format string for release tags.
+	TagFormat string `yaml:"tag_format,omitempty"`
+}
+
+// LegacyAPI represents an API in state.yaml.
+type LegacyAPI struct {
+	// Path is the API path relative to googleapis root.
+	Path string `yaml:"path"`
+
+	// ServiceConfig is the service config file name.
+	ServiceConfig string `yaml:"service_config,omitempty"`
+}
+
+// LegacyConfig represents the old .librarian/config.yaml file.
+type LegacyConfig struct {
+	// GlobalFilesAllowlist is the list of global files the container can access.
+	GlobalFilesAllowlist []GlobalFile `yaml:"global_files_allowlist,omitempty"`
+
+	// Libraries contains library-specific overrides.
+	Libraries []LegacyConfigLibrary `yaml:"libraries,omitempty"`
+}
+
+// GlobalFile represents a global file configuration.
+type GlobalFile struct {
+	// Path is the file path from repository root.
+	Path string `yaml:"path"`
+
+	// Permissions is the access mode (read-only, write-only, read-write).
+	Permissions string `yaml:"permissions"`
+}
+
+// LegacyConfigLibrary represents a library override in config.yaml.
+type LegacyConfigLibrary struct {
+	// ID is the library identifier.
+	ID string `yaml:"id"`
+
+	// NextVersion is the next version to release.
+	NextVersion string `yaml:"next_version,omitempty"`
+
+	// GenerateBlocked prevents generation.
+	GenerateBlocked bool `yaml:"generate_blocked,omitempty"`
+
+	// ReleaseBlocked prevents release.
+	ReleaseBlocked bool `yaml:"release_blocked,omitempty"`
+}
+
+// BuildBazelData represents parsed data from BUILD.bazel files.
+type BuildBazelData struct {
+	// Libraries maps library ID to BUILD.bazel metadata.
+	Libraries map[string]*BuildLibrary
+}
+
+// BuildLibrary represents BUILD.bazel metadata for a library.
+type BuildLibrary struct {
+	// ID is the library identifier.
+	ID string
+
+	// Transport is the transport protocol (grpc, rest, grpc+rest).
+	Transport string
+
+	// OptArgs are additional generator options.
+	OptArgs []string
+
+	// ServiceYAML is the service config file.
+	ServiceYAML string
+}
+
+// GeneratorInputData represents parsed data from .librarian/generator-input/.
+type GeneratorInputData struct {
+	// Files maps file names to their content.
+	Files map[string][]byte
+}
+
+// MergedLibrary represents a library with data merged from all sources.
+type MergedLibrary struct {
+	// Name is the library name (from state.yaml ID).
+	Name string
+
+	// APIs is the list of API paths.
+	APIs []string
+
+	// Keep is the list of files to preserve (from state.yaml preserve_regex).
+	Keep []string
+
+	// Transport is the transport protocol.
+	Transport string
+
+	// TagFormat is the release tag format.
+	TagFormat string
+
+	// PythonOptArgs are Python-specific generator options.
+	PythonOptArgs []string
+
+	// NextVersion is the next version to release.
+	NextVersion string
+
+	// GenerateBlocked prevents generation.
+	GenerateBlocked bool
+
+	// ReleaseBlocked prevents release.
+	ReleaseBlocked bool
+}
+
+// MergedData represents all merged library data.
+type MergedData struct {
+	// Libraries is the list of merged libraries.
+	Libraries []*MergedLibrary
+
+	// DefaultTransport is the most common transport.
+	DefaultTransport string
+
+	// DefaultTagFormat is the most common tag format.
+	DefaultTagFormat string
+
+	// DefaultOutput is the output path template.
+	DefaultOutput string
+}
